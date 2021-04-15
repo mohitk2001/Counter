830bf611ba68fbfd09f04660c906a2c548c69c57
mohit <mohitkumar.cse19@chitkarauniversity.edu.in>
2021-04-16 00:13:23 +0530

    first commit v1.0

---
+++ script.js
@@ -0,0 +1,35 @@
+var myvar;
+function fun()
+{
+     var date=document.getElementById("wish").value;
+     var date_ob=new Date(date);
+     
+     console.log(date_ob);
+     var day=0,hour=0,min=0,sec=0,milisec=0,totalsec=0;
+     myvar= setInterval(timer,1000);
+    function timer()
+    {
+        var current_date=new Date();
+        milisec=date_ob-current_date;
+        totalsec= Math.floor(milisec/1000);     
+        day=Math.floor(totalsec/3600/24);
+        hour=Math.floor(totalsec/3600) % 24;
+        min=Math.floor(totalsec/60) %60;
+        sec=Math.floor(totalsec)%60;
+        console.log(day,hour,min,sec);
+
+
+        document.querySelector("#d").textContent=day;
+        document.querySelector('#h').textContent=hour;
+        document.querySelector('#m').textContent=min;
+        document.querySelector('#s').textContent=sec;
+    }
+}
+function Reset()
+{
+    document.querySelector("#d").textContent=0;
+    document.querySelector('#h').textContent=0;
+    document.querySelector('#m').textContent=0;
+    document.querySelector('#s').textContent=0;
+    clearTimeout(myvar);
+}
